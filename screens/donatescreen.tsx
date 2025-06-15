292e4d031ed85da17ce2a150902939b81e5654d5
Egbetayo Damilola <76609799+Hezhykeyhel@users.noreply.github.com>
2025-06-15 14:13:25 +0100

    chore: added DonateModules submodule

---
+++ screens/donatescreen.tsx
@@ -0,0 +1,11 @@
+import { Box, Text } from "@/src/components/Restyle";
+
+export default function DonateScreen() {
+  return (
+    <Box flex={1} justifyContent="center" alignItems="center">
+      <Text textAlign="center" variant="header">
+        Home
+      </Text>
+    </Box>
+  );
+}
